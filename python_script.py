a398f0bffb7c961a94c40c306ffdbedd401ec850
Mathieu Busquet <62300166+MathieuBsqt@users.noreply.github.com>
2023-12-08 16:43:08 +0100

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -18,4 +18,4 @@ model_path = f'{model_path}{model_id}'
 # Available models = ['tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large-v3', 'large']
 
 # The whisper module’s load_model() method loads a whisper model in your Python application. You must pass the model name as a parameter to the load_model() method.
-model = whisper.load_model(model_id, download_root=model_id)
+model = whisper.load_model(model_id, download_root=model_path)
