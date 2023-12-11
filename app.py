797350ca69ea66e1a998ac37f6a723415e2cb384
Mathieu Busquet <62300166+MathieuBsqt@users.noreply.github.com>
2023-12-11 12:45:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,32 +8,31 @@ import io
 
 @st.cache_resource
 def load_model(model_id, model_path):
-    with st.spinner("Loading model..."):
-        # Check if two command-line arguments are provided
-        if len(sys.argv) !=3:
-            print("Usage: python app.py <whisper_model_id> <whisper_model_output_path>")
-            print("Example: python app.py large-v3 /workspace/whisper-model/")
-            sys.exit(1)
+    # Check if two command-line arguments are provided
+    if len(sys.argv) !=3:
+        print("Usage: python app.py <whisper_model_id> <whisper_model_output_path>")
+        print("Example: python app.py large-v3 /workspace/whisper-model/")
+        sys.exit(1)
+
+    # Check if the model path ends with '/'
+    if not model_path.endswith('/'):
+        model_path += '/'
+
+    # Get complete model file path
+    model_path = f'{model_path}{model_id}/{model_id}.pt'
     
-        # Check if the model path ends with '/'
-        if not model_path.endswith('/'):
-            model_path += '/'
+    # Define available device (CPU/GPU)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     
-        # Get complete model file path
-        model_path = f'{model_path}{model_id}/{model_id}.pt'
-        
-        # Define available device (CPU/GPU)
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        
-        # Load model on available device
-        model = whisper.load_model(model_path, device=device)
-        
-        # Display model's parameters in the app's logs
-        print(
-            f"Model will be run on {device}\n"
-            f"Model is {'multilingual' if model.is_multilingual else 'English-only'} "
-            f"and has {sum(np.prod(p.shape) for p in model.parameters()):,} parameters."
-        )
+    # Load model on available device
+    model = whisper.load_model(model_path, device=device)
+    
+    # Display model's parameters in the app's logs
+    print(
+        f"Model will be run on {device}\n"
+        f"Model is {'multilingual' if model.is_multilingual else 'English-only'} "
+        f"and has {sum(np.prod(p.shape) for p in model.parameters()):,} parameters."
+    )
 
     return model
     
