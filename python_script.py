0d98c9362680b63ebfdb55ca419613fa4591acdc
Mathieu Busquet <62300166+MathieuBsqt@users.noreply.github.com>
2023-12-11 09:40:54 +0100

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -1,19 +1,19 @@
 import sys
 
 # Check if at least one command-line argument is provided
-if len(sys.argv) !=2:
-    print("Usage: python main.py <whisper_model_output_path>")
-    print("Example: /workspace/whisper-model/")
+if len(sys.argv) !=3:
+    print("Usage: python main.py <whisper_model_id> <whisper_model_output_path>")
+    print("Example: python main.py large-v3 /workspace/whisper-model/")
     sys.exit(1)
 
 # Check if the model path ends with '/'
-model_path = sys.argv[1]
+model_path = sys.argv[2]
 if not model_path.endswith('/'):
     model_path += '/'
   
 ### Download the model in a local directory - Specify the version you want to use in the first parameter
 import whisper
-model_id = 'large-v3'
+model_id = sys.argv[1]
 model_path = f'{model_path}{model_id}'
 # Available models = ['tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large-v3', 'large']
 
